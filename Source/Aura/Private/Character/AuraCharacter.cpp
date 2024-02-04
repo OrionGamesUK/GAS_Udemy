a51411f0c5557fb2f17a665437824452e8027614
Jay Reid <Jay.Reid@Jagex.com>
2024-02-04 16:57:43 +0000

    The Character Classes

---
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,5 @@
+// Copyright Orion Games
+
+
+#include "Character/AuraCharacter.h"
+
---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Copyright Orion Games
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
